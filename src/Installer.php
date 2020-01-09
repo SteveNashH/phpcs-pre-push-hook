ccad655bdb49b7a275ea97ff497dc891934d67e5
rongpeng he <herongpeng1@gmail.com>
2020-01-09 16:19:58 +0800

    test

--- src/Installer.php
+++ src/Installer.php
@@ -5,7 +5,7 @@ class Installer
 {
     public static function postInstall()
     {
-        system('sh vendor/wickedreports/phpcs-pre-commit-hook/src/setup.sh');
+        system('sh vendor/stevenash/phpcd-pre-push-hook/src/setup.sh');
 //        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
 //
 //        } else {
